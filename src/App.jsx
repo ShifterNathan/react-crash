11aebe1984fba80f729d3a16e23f7f6a25bc5386
ShifterNathan <nathanielboydvillarroel@gmail.com>
2024-09-11 23:18:31 -0300

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 // import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards.jsx'
+import JobListings from './components/JobListings.jsx'
+import ViewAllJobs from './components/ViewAllJobs.jsx'
 
 const App = () => {
   return (
     <>
-    {/* <Navbar /> */}
-    <Hero />
-    <HomeCards  />
+      {/* <Navbar /> */}
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   )
 }
